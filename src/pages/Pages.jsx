5f52c4a256c2a6bbef3636631cb3870903356041
Olaowo5 <olaowolabi@hotmail.co.uk>
2023-10-30 01:50:21 +0100

    added category

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Homes from './Homes'
+import Cuisine from "./Cuisine";
+import {Route, Routes} from 'react-router-dom';
+import styled from 'styled-components';
 
 function Pages() {
   return (
-    <div>
-      <Homes />
-    </div>
+   
+    <Routes>
+      <Route path ="/" element = {<Homes />}/>
+      <Route path ="/cuisine/:type" element ={<Cuisine/>}/>
+    </Routes>
+   
   )
 }
+
+
 
 export default Pages
